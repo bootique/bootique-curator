b5eda3cf4f0564da9291697188d718ae00f9b982
Andrus Adamchik <andrus@objectstyle.com>
2015-12-08 19:42:43 +0300

    API cleanup

---
+++ src/main/java/com/nhl/zookeeper/ZookeeperBundle.java
@@ -0,0 +1,44 @@
+package com.nhl.zookeeper;
+
+import org.apache.curator.framework.CuratorFramework;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Provides;
+import com.nhl.launcher.config.ConfigurationFactory;
+
+public class ZookeeperBundle {
+
+	private static final String CONFIG_PREFIX = "zookeeper";
+
+	private String configPrefix;
+
+	public static Module zkModule() {
+		return zkModule(CONFIG_PREFIX);
+	}
+
+	public static Module zkModule(String configPrefix) {
+		return new ZookeeperBundle(configPrefix).module();
+	}
+
+	private ZookeeperBundle(String configPrefix) {
+		this.configPrefix = configPrefix;
+	}
+
+	public Module module() {
+		return new ZookeeperModule();
+	}
+
+	class ZookeeperModule implements Module {
+
+		@Override
+		public void configure(Binder binder) {
+			// do nothing.. configuration happens in @Provides
+		}
+
+		@Provides
+		public CuratorFramework createCurator(ConfigurationFactory configSource) {
+			return configSource.subconfig(configPrefix, ZookeeperConfig.class).createZkClient();
+		}
+	}
+}
