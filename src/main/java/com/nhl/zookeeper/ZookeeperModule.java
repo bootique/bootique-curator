83ef92eed244be698187ab4db4451931f6280ec9
Andrus Adamchik <andrus@objectstyle.com>
2015-12-08 17:17:07 +0300

    simplifying config

--- src/main/java/com/nhl/zookeeper/ZookeeperModule.java
+++ src/main/java/com/nhl/zookeeper/ZookeeperModule.java
@@ -4,7 +4,8 @@ import org.apache.curator.framework.CuratorFramework;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
-import com.google.inject.Singleton;
+import com.google.inject.Provides;
+import com.nhl.launcher.config.ConfigurationFactory;
 
 public class ZookeeperModule implements Module {
 
@@ -12,6 +13,11 @@ public class ZookeeperModule implements Module {
 
 	@Override
 	public void configure(Binder binder) {
-		binder.bind(CuratorFramework.class).toProvider(CuratorFrameworkProvider.class).in(Singleton.class);
+		// do nothing.. configuration happens in @Provides
+	}
+
+	@Provides
+	public CuratorFramework createCurator(ConfigurationFactory configSource) {
+		return configSource.subconfig(ZookeeperModule.CONFIG_PREFIX, ZookeeperConfig.class).createZkClient();
 	}
 }
