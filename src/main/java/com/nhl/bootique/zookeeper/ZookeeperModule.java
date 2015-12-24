7b959bf834f23ef28676ec8fa9ac085f44328157
Andrus Adamchik <andrus@objectstyle.com>
2015-12-24 20:24:27 +0300

    upgrading to BQ 0.10 and starting publishing to central

--- src/main/java/com/nhl/bootique/zookeeper/ZookeeperModule.java
+++ src/main/java/com/nhl/bootique/zookeeper/ZookeeperModule.java
@@ -3,22 +3,20 @@ package com.nhl.bootique.zookeeper;
 import org.apache.curator.framework.CuratorFramework;
 
 import com.google.inject.Provides;
-import com.nhl.bootique.FactoryModule;
-import com.nhl.bootique.factory.FactoryConfigurationService;
+import com.nhl.bootique.ConfigModule;
+import com.nhl.bootique.config.ConfigurationFactory;
 
-public class ZookeeperModule extends FactoryModule<ZookeeperClientFactory> {
+public class ZookeeperModule extends ConfigModule {
 
 	public ZookeeperModule(String configPrefix) {
-		super(ZookeeperClientFactory.class, configPrefix);
+		super(configPrefix);
 	}
 
 	public ZookeeperModule() {
-		super(ZookeeperClientFactory.class);
 	}
 
 	@Provides
-	public CuratorFramework createCurator(FactoryConfigurationService configService) {
-		return createFactory(configService).createZkClient();
+	public CuratorFramework createCurator(ConfigurationFactory configFactory) {
+		return configFactory.config(ZookeeperClientFactory.class, configPrefix).createZkClient();
 	}
-
 }
