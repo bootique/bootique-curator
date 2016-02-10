546b40d88d4f1165b1878cda123bebd998fcd878
Andrus Adamchik <andrus@objectstyle.com>
2016-02-10 14:25:49 +0300

    Adding Shutdown hook #1

--- src/main/java/com/nhl/bootique/zookeeper/ZookeeperModule.java
+++ src/main/java/com/nhl/bootique/zookeeper/ZookeeperModule.java
@@ -5,6 +5,8 @@ import org.apache.curator.framework.CuratorFramework;
 import com.google.inject.Provides;
 import com.nhl.bootique.ConfigModule;
 import com.nhl.bootique.config.ConfigurationFactory;
+import com.nhl.bootique.log.BootLogger;
+import com.nhl.bootique.shutdown.ShutdownManager;
 
 public class ZookeeperModule extends ConfigModule {
 
@@ -16,7 +18,15 @@ public class ZookeeperModule extends ConfigModule {
 	}
 
 	@Provides
-	public CuratorFramework createCurator(ConfigurationFactory configFactory) {
-		return configFactory.config(ZookeeperClientFactory.class, configPrefix).createZkClient();
+	public CuratorFramework createCurator(ConfigurationFactory configFactory, BootLogger bootLogger,
+			ShutdownManager shutdownManager) {
+		CuratorFramework client = configFactory.config(ZookeeperClientFactory.class, configPrefix).createZkClient();
+
+		shutdownManager.addShutdownHook(() -> {
+			bootLogger.trace(() -> "shutting down Curator...");
+			client.close();
+		});
+
+		return client;
 	}
 }
