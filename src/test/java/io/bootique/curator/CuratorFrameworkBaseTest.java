7f29ccdc8e3142a008eb52665a04e61a6afd5ace
Andrus Adamchik <andrus@objectstyle.com>
2020-07-19 10:42:12 +0300

    tests for ZK 3.6, dynamic ZK port mapping

--- src/test/java/io/bootique/curator/CuratorFrameworkBaseTest.java
+++ src/test/java/io/bootique/curator/CuratorFrameworkBaseTest.java
@@ -16,65 +16,62 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package io.bootique.curator;
 
-import com.github.dockerjava.api.command.CreateContainerCmd;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
+import io.bootique.BQCoreModule;
 import io.bootique.BQRuntime;
-import io.bootique.Bootique;
-import io.bootique.junit5.BQApp;
 import io.bootique.junit5.BQTest;
+import io.bootique.junit5.BQTestFactory;
+import io.bootique.junit5.BQTestTool;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.imps.CuratorFrameworkState;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.framework.recipes.shared.SharedCount;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.util.function.Consumer;
 
 import static org.junit.Assert.*;
 
+
 @BQTest
-@Testcontainers
-public class CuratorFrameworkFactoryIT {
+public abstract class CuratorFrameworkBaseTest {
 
-    private static final int HOST_PORT = 2181;
-    private static final int CONTAINER_EXPOSED_PORT = 2181;
-    private static final Consumer<CreateContainerCmd> MAPPING_CMD =
-            e -> e.getHostConfig().withPortBindings(
-                    new PortBinding(
-                            Ports.Binding.bindPort(HOST_PORT),
-                            new ExposedPort(CONTAINER_EXPOSED_PORT)
-                    )
-            );
+    protected static GenericContainer createZookeeper(String container) {
+        return new GenericContainer(container).withExposedPorts(2181);
+    }
 
-    @Container
-    public static GenericContainer zookeeper = new GenericContainer("zookeeper:3.6")
-            .withCreateContainerCmdModifier(MAPPING_CMD)
-            .withExposedPorts(CONTAINER_EXPOSED_PORT);
+    @BQTestTool
+    private BQTestFactory testFactory = new BQTestFactory();
 
-    @BQApp(skipRun = true)
-    static final BQRuntime app = Bootique.app("--config=classpath:config.yml")
-            .module(new CuratorModule())
-            .createRuntime();
+    protected BQRuntime createRuntime() {
+        int port = getZk().getMappedPort(2181);
+        return testFactory
+                .app()
+                .autoLoadModules()
+                .module(b -> BQCoreModule.extend(b).setProperty("bq.curator.connectString", "localhost:" + port))
+                .createRuntime();
+    }
+
+    protected abstract GenericContainer getZk();
 
     @Test
-    public void testFrameworkState() {
-        CuratorFramework framework = app.getInstance(CuratorFramework.class);
+    public void testCurator() throws Exception {
+        CuratorFramework framework = createRuntime().getInstance(CuratorFramework.class);
+
+        testFrameworkState(framework);
+        testSharedCounter(framework);
+        testMutex(framework);
+    }
+
+
+    private void testFrameworkState(CuratorFramework framework) {
 
         assertNotNull(framework);
         assertSame(framework.getState(), CuratorFrameworkState.STARTED);
     }
 
-    @Test
-    public void testSharedCounter() throws Exception {
-        CuratorFramework framework = app.getInstance(CuratorFramework.class);
+
+    private void testSharedCounter(CuratorFramework framework) throws Exception {
 
         // test is only about correct Curator integration, so we do nothing meaningful here
         try (SharedCount count = new SharedCount(framework, "/test_counter", 123)) {
@@ -85,10 +82,8 @@ public class CuratorFrameworkFactoryIT {
             assertEquals(321, count.getCount());
         }
     }
-
-    @Test
-    public void testMutex() throws Exception {
-        CuratorFramework framework = app.getInstance(CuratorFramework.class);
+    
+    private void testMutex(CuratorFramework framework) throws Exception {
 
         // test is only about correct Curator integration, so we do nothing meaningful here
         InterProcessMutex mutex = new InterProcessMutex(framework, "/test_mutex");
