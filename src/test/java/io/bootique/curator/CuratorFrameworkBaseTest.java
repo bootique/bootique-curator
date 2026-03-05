ae17675524ef8f1fe04dc97f3a117636f0dbe3de
Andrus Adamchik <andrus@objectstyle.com>
2026-03-05 13:40:37 -0500

    Upgrading to modules-parent 4.0.5

--- src/test/java/io/bootique/curator/CuratorFrameworkBaseTest.java
+++ src/test/java/io/bootique/curator/CuratorFrameworkBaseTest.java
@@ -20,9 +20,9 @@ package io.bootique.curator;
 
 import io.bootique.BQCoreModule;
 import io.bootique.BQRuntime;
-import io.bootique.junit5.BQTest;
-import io.bootique.junit5.BQTestFactory;
-import io.bootique.junit5.BQTestTool;
+import io.bootique.junit.BQTest;
+import io.bootique.junit.BQTestFactory;
+import io.bootique.junit.BQTestTool;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.imps.CuratorFrameworkState;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
